c790e475c80248f20ccd60e6e659886950651c8f
Lincoln Aguiar <lincolnalca@gmail.com>
2023-11-18 11:38:50 -0300

    adding address

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Company extends Model
 {
@@ -16,8 +18,13 @@ class Company extends Model
         'cnpj',
     ];
 
-    public function users()
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function address(): HasOne
+    {
+        return $this->hasOne(Address::class);
     }
 }
